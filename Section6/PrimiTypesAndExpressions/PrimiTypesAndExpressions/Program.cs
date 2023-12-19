07c57a444ca19eccf397034d30a907d6e39e8f1c
saraswathy <saraswathy@gmail.com>
2023-12-19 15:29:17 +0530

    Add project files.

---
+++ Section6/PrimiTypesAndExpressions/PrimiTypesAndExpressions/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimiTypesAndExpressions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DoApp();
+        }
+        static public void DoApp()
+        {
+
+            bool xbool = true;
+            byte xbyte = 250;
+            sbyte xsbyte = -100;
+            char xchar = 'a';
+            decimal xdecimal = 87.9M;
+            double xdouble = 651.09;
+            float xfloat = 465.1f;
+            int xint = -8909;
+            uint xuint = 91881;
+            long xlong = -1313131312;
+            ulong xulong = 01919181;
+            object xobject = new object();
+            short xshort = -19;
+            ushort xushort = 19;
+            string xstring = "name";
+
+            if (xbool == true && xbyte < 300)
+                Console.WriteLine("This expression evaluates to true");
+            if (xshort > 6 || xint > -9000)
+                Console.WriteLine("This expression evaluates to false");
+            int sum = xint + (int)xulong;
+            double product = xdouble * xfloat;
+            decimal difference = xdecimal - (decimal)xshort;
+            Console.WriteLine("Values after operations:");
+            Console.WriteLine($"xbool: {xbool}");
+            Console.WriteLine($"xbyte: {xbyte}");
+            Console.WriteLine($"xsbyte: {xsbyte}");
+            Console.WriteLine($"xchar: {xchar}");
+            Console.WriteLine($"xdecimal: {xdecimal}");
+            Console.WriteLine($"xdouble: {xdouble}");
+            Console.WriteLine($"xfloat: {xfloat}");
+            Console.WriteLine($"xint: {xint}");
+            Console.WriteLine($"xuint: {xuint}");
+            Console.WriteLine($"xlong: {xlong}");
+            Console.WriteLine($"xulong: {xulong}");
+            Console.WriteLine($"xobject: {xobject}");
+            Console.WriteLine($"xshort: {xshort}");
+            Console.WriteLine($"xushort: {xushort}");
+            Console.WriteLine($"xstring: {xstring}");
+
+            Console.WriteLine($"Sum of xint and xulong: {sum}");
+            Console.WriteLine($"Product of xdouble and xfloat: {product}");
+            Console.WriteLine($"Difference of xdecimal and xshort: {difference}");
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
